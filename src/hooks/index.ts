680f19bf35894123e62b5a050bd918c538d3e9ba
beinyi <eponchincev2011@gmail.com>
2025-06-03 12:47:09 +0700

    solution: flowSlice, useFlowStore

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,4 @@
-export * from './useAppDispatch';
-export * from './useAppSelector';
-export * from './useGraph';
+export * from "./useAppDispatch";
+export * from "./useAppSelector";
+// export * from './useGraph';
+export * from "./useFlowStore";
